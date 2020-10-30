28bec01f90f9f1eb0120a7474e20205b179812e4
sebhatgg <38915552+sebhatgg@users.noreply.github.com>
2020-10-30 14:17:15 -0400

    Add files via upload

---
+++ Lotto.py
@@ -0,0 +1,10 @@
+from random import seed
+from random import random
+from random import randint
+
+for range in range(5):
+    value=randint(1,70)
+    print (value)
+
+print("\n")
+print(randint(1,25))
